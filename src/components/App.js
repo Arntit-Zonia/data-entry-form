3e56737ec0726848f66e1990c1b3d551687a5d5d
Arntit-Zonia <arditzonia@gmail.com>
2019-10-16 14:15:54 +0100

    - basic file structure implemented - dependencies installed

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Details from "./Details";
+
+function App() {
+	return (
+		<div className="App">
+			<Details />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { reducer as formReducer } from "redux-form";
+import { combineReducers } from "redux";
+
+export default combineReducers({ form: formReducer });
